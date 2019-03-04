d77a6835372aa586feb726379fa3c04411f531e2
Kei Sato <ckeisato@gmail.com>
2019-03-03 18:40:19 -0800

    Update gulp tasks and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,41 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
 var browserSync = require('browser-sync').create();
 var clean = require('gulp-clean');
 var cssmin = require('gulp-cssmin');
-var flatten = require('gulp-flatten');
-var rename = require('gulp-rename');
 var htmlmin = require('gulp-htmlmin');
 
-var paths = {
-	'node': './node_modules',
-	'assets': './assets'
-}
+gulp.task('clean', () => gulp.src('./dist/**/**/*', {read: false})
+	.pipe(clean())
+);
 
-// remove files in the public folder
-gulp.task('clean', function(){
-	return gulp.src('./public/**/**/*', {read: false})
-		.pipe(clean());
-});
+gulp.task('html', () => gulp.src('src/*.html')
+	.pipe(htmlmin({
+		collapseWhitespace: true
+	}))
+	.pipe(gulp.dest('./dist'))
+);
 
-gulp.task('serve', function(){
+gulp.task('css', () => gulp.src('src/style.css')
+	.pipe(cssmin())
+	.pipe(gulp.dest('./dist'))
+);
+
+gulp.task('js', () => gulp.src('src/*.js').pipe(gulp.dest('./dist')));
+
+gulp.task('serve', () => {
 	browserSync.init({
 		server: {
-			baseDir: './public'
+			baseDir: './dist'
 		}
 	});
 
-	gulp.watch(paths.assets + '/pages/*' , ['pages']);
-	gulp.watch(paths.assets + '/images/*' , ['images']);
-	gulp.watch(paths.assets + '/styles/**/*.scss',['styles']);
-	gulp.watch(paths.assets + '/data/*', ['data']);
-	gulp.watch(paths.assets + '/scripts/*.js',['scripts']);
-
-  gulp.watch([paths.assets + '/data/*', paths.assets + '/styles/app.scss',
-    'public/*.html', paths.assets + '/scripts/**/*.js']).on('change', browserSync.reload);
+	gulp.watch('src/*.html', gulp.series('html'));
+	gulp.watch('src/*.js', gulp.series('js'));
+	gulp.watch('src/*.css',gulp.series('css'));
+	gulp.watch('src/*').on('change', browserSync.reload);
 });
 
+gulp.task('build', gulp.series(['html', 'js', 'css']));
 
-gulp.task('pages', function(){
-	return gulp.src(paths.assets + '/pages/*')
-		.pipe(htmlmin({
-			collapseWhitespace: true
-		}))
-		.pipe(gulp.dest('./public'), { base: '.' });
+gulp.task('default', gulp.series(['build', 'serve']));
 
-});
-
-// compiles styles with foundation base styles
-gulp.task('styles', function(){
-	gulp.src(paths.assets + '/styles/app.scss')
-	.pipe(sass())
-	.pipe(cssmin())
-	.pipe(gulp.dest('./public/css'), { base: '.'});
-});
-
-
-gulp.task('images', function(){
-	return gulp.src([
-			paths.assets + '/images/**/*',
-			paths.assets + '/images/*'
-		])
-		.pipe(flatten())
-		.pipe(gulp.dest('./public/assets/'));
-});
-
-gulp.task('documents', function(){
-	return gulp.src(paths.assets + '/documents/*')
-		.pipe(gulp.dest('./public/assets'));
-})
-
-
-gulp.task('data', function(){
-	return gulp.src(paths.assets + '/data/*')
-		.pipe(gulp.dest('./public/data'));
-})
-
-gulp.task('scripts', function(){
-
-	// index page
-	gulp.src(paths.assets + '/scripts/index.js')
-		.pipe(gulp.dest('./public/js'));
-
-	// artwork page
-	gulp.src(paths.assets + '/scripts/artApp.js')
-		.pipe(gulp.dest('./public/js'));
-
-});
-
-gulp.task('cname', function() {
-	gulp.src('./CNAME').pipe(gulp.dest('./public'));
-});
-
-gulp.task('default', ['pages', 'images', 'styles', 'data', 'scripts', 'documents', 'serve']);
-
-gulp.task('build', ['pages', 'images', 'styles', 'data', 'scripts', 'documents', 'cname']);
